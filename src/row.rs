019616b5197743cf715f58db44b47a3e53b4c416
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2023-06-09 22:22:27 +0200

    better number coloring

--- src/row.rs
+++ src/row.rs
@@ -1,6 +1,6 @@
 use crate::{highlighting, SearchDirection};
 use std::cmp;
-use termion::color::{self, Color};
+use termion::color;
 use unicode_segmentation::UnicodeSegmentation;
 
 #[derive(Default)]
@@ -185,6 +185,7 @@ impl Row {
             }
         }
 
+        let mut prev_is_separator = true;
         let mut index = 0;
         while let Some(c) = chars.get(index) {
             if let Some(word) = word {
@@ -197,14 +198,26 @@ impl Row {
                 }
             }
 
-            if c.is_ascii_digit() {
+            let previous_highlight = if index > 0 {
+                #[allow(clippy::integer_arithmetic)]
+                highlighting
+                    .get(index - 1)
+                    .unwrap_or(&highlighting::Type::None)
+            } else {
+                &highlighting::Type::None
+            };
+
+            if c.is_ascii_digit()
+                && (prev_is_separator || previous_highlight == &highlighting::Type::Number)
+                || (c == &'.' && previous_highlight == &highlighting::Type::Number)
+            {
                 highlighting.push(highlighting::Type::Number);
             } else {
                 highlighting.push(highlighting::Type::None);
             }
+            prev_is_separator = c.is_ascii_punctuation() || c.is_ascii_whitespace();
             index += 1;
         }
-
         self.highlighting = highlighting;
     }
 
